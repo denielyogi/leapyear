9ab2e1e72fef96f6b5a92e1e155d8cdd1a2a030c
MohitBaghel1213 <69578354+MohitBaghel1213@users.noreply.github.com>
2020-10-01 00:09:20 +0530

    Update leapyear.php

--- leapyear.php
+++ leapyear.php
@@ -6,3 +6,20 @@
     </form>  
 </body>  
 </html>  
+<?php   
+    if($_POST)  
+    {     
+         
+        $year = $_POST['year'];   
+        
+       
+        if( (0 == $year % 4) and (0 != $year % 100) or (0 == $year % 400) )  
+        {  
+            echo "$year is a Leap Year";    
+        }  
+        else  
+        {  
+            echo "$year is not a Leap Year";    
+        }  
+    }   
+?> 
